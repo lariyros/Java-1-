10a29f09893286bc47e429681d95fcf8cf8bcefb
unknown <larissa.ros.silva1a@gmail.com>
2025-04-02 14:56:27 -0300

    Exibir a mensagem 10 vezes

---
+++ Ex14.java
@@ -0,0 +1,10 @@
+/******************************************************************************
+14 - Exibir a mensagem 10 vezes
+*******************************************************************************/
+public class Ex14 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Eu gosto de estudar Algoritmos!");
+        }
+    }
+}
